21a3f19b0848e5c80705e2adb92e59c2a0a9dd08
Still <git@stillconfusing.net>
2022-06-17 21:18:57 -0400

    changed colors to match new keycaps

--- keyboards/ymd75/keymaps/areyouconfused/keymap.c
+++ keyboards/ymd75/keymaps/areyouconfused/keymap.c
@@ -22,9 +22,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_QWERTY] = LAYOUT(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,       KC_F12,    KC_HOME,  KC_END,  KC_DEL,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,       KC_MINS,      KC_EQL,    KC_BSPC,           KC_INS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_LBRC,      KC_RBRC,   KC_BSLS,           TG(_NUM),
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,       KC_F12,    KC_PSCR,  KC_SCROLLLOCK,  KC_PAUSE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,       KC_MINS,      KC_EQL,    KC_BSPC,           TG(_NUM),
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_LBRC,      KC_RBRC,   KC_BSLS,           KC_HOME,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN ,   KC_QUOT,      KC_ENT,                       KC_PGUP,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,    KC_RSFT,                 KC_UP,             KC_PGDN,
         KC_LCTL,  KC_LGUI,  KC_LALT,                  KC_SPC,                           KC_RALT,  MO(_FN),  LT(_SMACRO, KC_RCTL),     KC_LEFT,   KC_DOWN,           KC_RGHT
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT(
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_SLASH,  KC_KP_ASTERISK,  KC_KP_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_7,  KC_KP_8,      KC_KP_9,         KC_KP_PLUS,   KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4,  KC_KP_5,      KC_KP_6,         KC_KP_PLUS,   KC_TRNS,  KC_TRNS,  KC_TRNS,            TG(_NUM),
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_7,  KC_KP_8,      KC_KP_9,         KC_KP_PLUS,   KC_TRNS,  KC_TRNS,  KC_TRNS,            TG(_NUM),
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4,  KC_KP_5,      KC_KP_6,         KC_KP_PLUS,   KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_1,  KC_KP_2,      KC_KP_3,         KC_KP_ENTER,  KC_TRNS,  KC_TRNS,                      KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_0,      KC_KP_DOT,         KC_KP_ENTER,  KC_TRNS,                      KC_TRNS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                      KC_TRNS,      KC_TRNS,         KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT(
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_END,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MNXT,       KC_TRNS,                 KC_TRNS,
         KC_TRNS,  RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MPLY,                      KC_TRNS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,  KC_TRNS,  OSL(_TOGG),            KC_TRNS,  KC_TRNS,  KC_TRNS
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-#define RGB_BASE_COLOR 128, 255, 111
+#define RGB_BASE_COLOR 0, 255, 200
 #define HSV_cORANGE 10, 255, 255
 
 #define LAYER_LED 11
@@ -85,7 +85,7 @@ const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 );
 
 const rgblight_segment_t PROGMEM mock_rgb[] = RGBLIGHT_LAYER_SEGMENTS(
-    {6, 3, HSV_RED}
+    {6, 3, HSV_CYAN}
 );
 
 const rgblight_segment_t PROGMEM die_rgb[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -94,7 +94,7 @@ const rgblight_segment_t PROGMEM die_rgb[] = RGBLIGHT_LAYER_SEGMENTS(
 
 // fn layer's rgb
 const rgblight_segment_t PROGMEM fn1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {LAYER_LED, LAYER_LED_COUNT, HSV_RED}
+    {LAYER_LED, LAYER_LED_COUNT, HSV_BLUE}
 );
 
 const rgblight_segment_t PROGMEM fn2_layer[] = RGBLIGHT_LAYER_SEGMENTS( {LAYER_LED, LAYER_LED_COUNT, HSV_ORANGE}
